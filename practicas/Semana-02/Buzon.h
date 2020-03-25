dfd6fe9495d1b71fc8cbd11af0224588bddc5df6
farroyo-ucr <farroyom@gmail.com>
2020-03-25 10:53:45 -0600

    Agregando ejemplos

--- practicas/Semana-02/Buzon.h
+++ practicas/Semana-02/Buzon.h
@@ -21,7 +21,7 @@ class Buzon {
    public:
       Buzon();
       ~Buzon();
-      int Enviar( char * mensaje, long tipo );	// Envia la tira de caracteres como un mensaje tipo
+      int Enviar( const char * mensaje, int veces, long tipo );	// Envia la tira de caracteres como un mensaje tipo
       int Enviar( void * mensaje, int len, int veces, long tipo );
       int Recibir( char * mensaje, int len, long tipo );   // len es el tamaño máximo que soporte la variable mensaje
       int Recibir( void * mensaje, int len, long tipo );   // mensaje puede ser de tipo msgbuf
