8f93159a3bef956edcb29effaaa432603b83df9d
farroyo-ucr <farroyom@gmail.com>
2020-03-25 10:52:57 -0600

    Agregando ejemplos

---
+++ practicas/Semana-02/enviarConClases.cc
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <string.h>
+
+#include "Buzon.h"
+
+const char * html_labels[] = {
+	"abc",
+	"bcd",
+	"cdef",
+	"defg",
+	"efgh",
+	"li",
+	""
+};
+
+int main( int argc, char ** argv ) {
+   int id, i, size, st;
+   Buzon m;
+
+   i = 0;
+   while ( strlen( html_labels[ i ] ) ) {
+      st = m.Enviar( html_labels[ i ], i, 2020 );	// Send a message with 2020 type (label, n)
+      printf( "Label: %s, status %d \n", html_labels[ i ], st );
+   }
+
+}
+
