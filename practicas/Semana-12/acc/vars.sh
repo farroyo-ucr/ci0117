8fe22594ec99d1195b60ec461b4d513b8726a78e
farroyo-ucr <farroyom@gmail.com>
2020-06-30 15:36:14 -0600

    vars.h para Acc

---
+++ practicas/Semana-12/acc/vars.sh
@@ -0,0 +1,10 @@
+#
+# Puede correr este archivo con: "source vars.sh"
+# para lograr obtener las rutas a los compiladores
+# y los manuales de OpenAcc
+#
+#  Es dependiente de la distribución de Linux
+#
+
+export PATH=$PATH:/opt/pgi/linux86-64-llvm/19.10/bin/
+export MANPATH=$MANPATH:/opt/pgi/linux86-64-llvm/2019/man/
